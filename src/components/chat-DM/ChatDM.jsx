ca5b908bc7efa435b7c877da6cb84a9e35fe49c6
SamiSelx <samifcb14@gmail.com>
2024-07-09 00:06:05 +0200

    edit url

--- src/components/chat-DM/ChatDM.jsx
+++ src/components/chat-DM/ChatDM.jsx
@@ -25,7 +25,7 @@ export default function ChatDM(){
       async function getConversation(){
           const endpoint = isRoom ? 'room/'+recieverId :recieverId 
           try {
-            const response = await fetch('http://localhost:5000/api/message/'+endpoint,{
+            const response = await fetch('https://chat-app-backend-575t.onrender.com/api/message/'+endpoint,{
               method:'GET',
               headers:{
                   'content-type':'application/json',
@@ -66,7 +66,7 @@ export default function ChatDM(){
         // fetch all users
         async function getUsers(){
             try {
-              const response = await fetch('http://localhost:5000/api/user',{
+              const response = await fetch('https://chat-app-backend-575t.onrender.com/api/user',{
                 method:'GET',
                 headers:{
                     'content-type':'application/json',
@@ -120,7 +120,7 @@ export default function ChatDM(){
         formData.append('message',message.message)
         formData.append('file',message.file)
         try {
-          const response = await fetch(`http://localhost:5000/api/message/send/${endpoint}`,{
+          const response = await fetch(`https://chat-app-backend-575t.onrender.com/api/message/send/${endpoint}`,{
             method:'POST',
             body:formData,
             headers:{
@@ -146,7 +146,7 @@ export default function ChatDM(){
       async function createAndJoinRoom(e){
         const endpoint = e.target.id
         if(room == "") return
-        const response = await fetch('http://localhost:5000/api/room/'+endpoint,{
+        const response = await fetch('https://chat-app-backend-575t.onrender.com/api/room/'+endpoint,{
             method:'POST',
             body:JSON.stringify({roomName:room}),
             headers:{
@@ -222,12 +222,12 @@ export default function ChatDM(){
                       </p>
                       {msg.message.filePath && (
                         <a
-                          href={"http://localhost:5000" + msg.message.filePath}
+                          href={"https://chat-app-backend-575t.onrender.com" + msg.message.filePath}
                           target="_blank"
                           rel="noopener noreferrer"
                           className="cursor-pointer w-full h-full"
                         >
-                          <img src={"http://localhost:5000" + msg.message.filePath} onError={e => e.target.src = './vite.svg'} className="h-1/3 w-1/3"/>
+                          <img src={"https://chat-app-backend-575t.onrender.com" + msg.message.filePath} onError={e => e.target.src = './vite.svg'} className="h-1/3 w-1/3"/>
                         </a>
                       )}
                     </div>
